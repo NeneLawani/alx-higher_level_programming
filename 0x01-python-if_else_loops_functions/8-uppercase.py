6f6e23d3d05b0e1b820f1cdf1babbc7671815d04
NeneLawani <nene@lawani.com>
2023-05-09 16:25:11 -0700

    8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new = ''
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            new = new + chr(ord(i) - 32)
+        else:
+            new = new + i
