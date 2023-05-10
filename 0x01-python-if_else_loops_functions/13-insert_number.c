0ac0280468054634bb16efc641c7f3de463487be
NeneLawani <nene@lawani.com>
2023-05-10 10:49:06 -0700

    13. Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,42 +2,41 @@
 
 /**
  * insert_node - inserts a number into a sorted singly linked list
- * @head: pointer to the head of the list
- * @number: number to be inserted
- *
+ * @head: pointer to pointer to the head of the linked list
+ * @number: the number to insert
  * Return: the address of the new node, or NULL if it failed
  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *current;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *current;
 
-	if (head == NULL)
-		return (NULL);
-
-	/* create the new node */
-	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = number;
 	new_node->next = NULL;
 
-	/* special case for an empty list */
 	if (*head == NULL)
 	{
 		*head = new_node;
-		(*head)->next = NULL;
 		return (new_node);
 	}
 
-	/* traverse the list to find the insertion point */
+	if (number < (*head)->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
 	current = *head;
 	while (current->next != NULL && current->next->n < number)
 	{
 		current = current->next;
 	}
 
-	/* insert the new node */
 	new_node->next = current->next;
 	current->next = new_node;
 
