1f1fd4bfca4b8a6fa22402c618cb39a22e8f6cd3
NeneLawani <nene@lawani.com>
2023-05-11 13:09:30 -0700

    1. My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    funcs = [('+', add), ('-', sub), ('*', mul), ('/', div)]
+    for func in funcs:
+        print('{} {} {} = {}'.format(a, func[0], b, func[1](a, b)))
