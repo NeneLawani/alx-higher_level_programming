351dba73d63854664391205f853e9c4cc1401b5c
NeneLawani <nene@lawani.com>
2023-05-14 17:47:24 -0700

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
-    return (length, first)
+    if sentence:
+        length = len(sentence)
+        first = sentence[0]
+        return (length, first)
