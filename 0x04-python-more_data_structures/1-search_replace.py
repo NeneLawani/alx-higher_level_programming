5f613318e653015230867a050208f28552a07e26
NeneLawani <nene@lawani.com>
2023-05-17 11:30:22 -0700

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return
     new_list = [replace if num == search else num for num in my_list]
     return new_list
